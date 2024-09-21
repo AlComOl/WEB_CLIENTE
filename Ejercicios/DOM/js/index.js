47e8d48214a50a92e2c6de47c2aec622eebe13a3
Alvaro <alvcomoli@alu.edu.gva.es>
2024-09-21 11:24:41 +0200

    DOM

--- Ejercicios/DOM/js/index.js
+++ Ejercicios/DOM/js/index.js
@@ -1,11 +1,40 @@
 
 /*Número de enlaces de la página*/
 window.onload = function() {
-/*var enlace=document.getElementsByTagName('a');
-alert(enlace.length);
-/*Dirección a la que enlaza el penúltimo enlace
-var segundo=document.getElementsByTagName('a');
-alert(segundo.length-2);*/
-/*Numero de enlaces que enlazan a http://prueba*/
-let enlazan=document.getElementsByName(a.href=" http://prueba");
+    var info = document.getElementById("informacion");
+   
+
+ // Numero de enlaces de la pagina
+
+  var enlaces = document.getElementsByTagName("a");
+  info.innerHTML = "Numero de enlaces = "+enlaces.length;
+
+/*Dirección del penúltimo enlace*/
+
+var mensaje = "El penultimo enlace apunta a: "+enlaces[enlaces.length-2].href;
+info.innerHTML = info.innerHTML + "<br/>" + mensaje;
+
+
+// Numero de enlaces que apuntan a http://prueba
+var c=0;
+for(var i=0;i<enlaces.length;i++){
+    if(enlaces[i].href=="http://prueba" || enlaces[i].href=="http://prueba/" ){
+      
+        c++;
+    }
 }
+
+
+info.innerHTML=info.innerHTML+"<br>"+ "Número de mensajes que apuntan a http://prueba = "+ c; 
+
+
+
+//Número de enlaces del tercer párrafo
+
+var parrafos= document.getElementsByTagName("p");
+
+enlaces=parrafos[2].getElementsByTagName("a");
+
+info.innerHTML=info.innerHTML+"<br/>"+"Numero de enlaces del tercer parrafo"+ enlaces.length;
+}
+
