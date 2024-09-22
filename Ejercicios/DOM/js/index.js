64556caae8bcc0c468bd6cb58ca25c9f163afb69
Alvaro <alvcomoli@alu.edu.gva.es>
2024-09-22 22:41:40 +0200

    Ejercicio de Ana no terminado

---
+++ Ejercicios/DOM/js/index.js
@@ -0,0 +1,15 @@
+
+/*Número de enlaces de la página*/
+window.onload = function() {
+console.log("Hola");
+var enlace=document.getElementsByTagName('a');
+alert(enlace.length);
+/*Dirección a la que enlaza el penúltimo enlace*/
+var segundo=document.getElementsByTagName('a');
+alert(segundo[6]);
+/*Numero de enlaces que enlazan a http://prueba*/
+var enlazan =document.getElementsByName(segundo[6]);
+alert(enlazan.length);
+/*Número de enlaces del tercer párrafo*/
+
+}
