d46938734bcac9ccf31c8a5b33f108977ac3ab13
Alvaro <alvcomoli@alu.edu.gva.es>
2024-09-23 13:09:48 +0200

    probando

---
+++ Ejercicios/DOM/Ejercicio14/js/index.js
@@ -0,0 +1,24 @@
+/*A partir de la página web proporcionada, completar el código JavaScript para que:
+
+    Cuando se pinche sobre el primer enlace, 
+    se oculte su sección relacionada
+    Cuando se vuelva a pinchar sobre el mismo enlace, se muestre otra vez esa sección de contenidos
+    Completar el resto de enlaces de la página para que su comportamiento sea idéntico al del primer enlace
+    Cuando una sección se oculte, debe cambiar el mensaje del enlace asociado (pista: propiedad innerHTML)
+*/ 
+
+window.onload=function(){
+    let contenido=document.getElementsById("contenidos_");
+    for(i=0;i<contenido;i++){
+        contenido.
+
+    }
+
+
+console.log(contenido);
+    
+}
+
+
+
+
---
+++ Ejercicios/DOM/florencia-plantilla/js/index.js
@@ -0,0 +1,15 @@
+let h2etiqueta=document.getElementsByh2TagName("h2");
+
+
+for(i=0;i<h2etiqueta.length;i++){
+
+    let parrafo= h2etiqueta[1];
+
+    document.write(parrafo);
+
+}
+
+let p=document.getElementsByTagName(p);
+
+
+
