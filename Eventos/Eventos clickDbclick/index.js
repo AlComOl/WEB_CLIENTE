623394fd2cc9b693c6eea0fafeb58ba0a2262f71
Alvaro <alvcomoli@alu.edu.gva.es>
2024-09-26 14:57:51 +0200

    ana

---
+++ Eventos/Eventos clickDbclick/index.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById("jk").addEventListener(dbclick,prueba);
+
+
+function prueba(){
+   let num=document.getElementById("reinicio").addEventListener(click,reiniciar);
+
+   function prueba(){
+    let num=document.getElementById("cuentas");
+    let por2=num.textContent*2;
+    num.innerHTML=por2.textContent;
+   }
+
+   
+      
+}
