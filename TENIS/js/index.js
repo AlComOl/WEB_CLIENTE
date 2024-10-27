9a07810a61e0806ff4cfcf6a787855e28f42b0f2
Alvaro <alvcomoli@alu.edu.gva.es>
2024-10-27 22:54:53 +0100

    bona nit tennis

--- TENIS/js/index.js
+++ TENIS/js/index.js
@@ -1,7 +1,11 @@
 window.onload=function(){
 
 
-    document.getElementById("empezarPartida").addEventListener("click",inicioJuego);
+    document.getElementById('empezarPartida').addEventListener('click',inicioJuego);
+
+
+   
+
 
 
 
@@ -16,21 +20,51 @@ window.onload=function(){
 
 
 
-
+//elimina la pantalla del formulario de datos requeridos y muestra el marcador
     function inicioJuego(){
 
         var pInicio= document.getElementById("datos");//declara una variable introducciendo los elemetos(container) en ella
     
-        pInicio.parentNode.removeChild(pInicio);//borra los elementos de dentro de la variable
+        pInicio.style.display="none";
     
-        var pJuego=document.getElementById("juego");
-    
-        pJuego.style.display="block";//muestra la pantalla, cambia el estado de display
+        // Cogemos los id del ckeeckbox
+        let set3=document.getElementById('set3');
+        let set5=document.getElementById('set5');
+        // las secciones donde esta alojado la pantalla del marcador de 3 y 5 sets
+        let contenedorSet3=document.getElementById('displaySets3');
+        let contenedorSet5=document.getElementById('displaySets5');
+        // Si esta chequeado el 3 muestra pantalla del 3 y al contrario
+        if(set3.checked){
+            contenedorSet3.style.display="block";
+            contenedorSet5.style.display = "none"; 
+        }else if(set5.checked){
+            contenedorSet3.style.display="none";
+            contenedorSet5.style.display="block"; 
+        }
 
+// Modifica en el encabezado el Nombre de torneo 
+        let torneoset3=document.getElementById('torneoset3');
+        torneoset3.textContent=document.getElementById('inputTorneo').value;
 
+        let torneset5=document.getElementById('torneoset5');
+        torneset5.textContent=document.getElementById('inputTorneo').value;
+//Hay que meter los campos de los datos de html de la misma forma que lo hacemos anterior mente
+        let jugador1=document.getElementById()
 
-    
     }
+
+
+
+
+    // Creamos el objeto Jugador 
+
+
+    class Jugador1{
+
+         Ace = 0;
+
+    }
+
 }
 
 
